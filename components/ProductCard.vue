00472af78c7e9aec58e2dd92897e7fecda3a5c60
mkubincova <mkubincova@proton.me>
2024-04-25 11:54:24 +0200

    feat: error pages

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="card text-center">
+    <div class="card text-center pb-5">
         <img :src="product.image" alt="product thumb" class="thumb">
         <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
-        <NuxtLink :to="`/products/${product.id}`">
-            <p class="btn my-4">View details</p>
-        </NuxtLink>
+        <NuxtLink :to="`/products/${product.id}`" class="btn">View details</NuxtLink>
     </div>
 </template>
 
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -13,6 +13,10 @@ definePageMeta({
 
 // fetch products
 const { data: product } = await useFetch('https://fakestoreapi.com/products/' + id);
+
+if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true });
+}
 </script>
 
 <style lang="scss" scoped></style>
